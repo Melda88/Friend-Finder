4771c3bf2bac897816f05ce1c8c21087022d54ea
Alex Melda <melda.alex@gmail.com>
2017-11-24 19:23:37 -0500

    first

---
+++ app/data/friends.js
@@ -0,0 +1,24 @@
+var friends = [
+	{
+		name: 'Leonardo DiCaprio',
+		photo: 'https://en.wikipedia.org/wiki/Leonardo_DiCaprio#/media/File:Leonardo_DiCaprio_October_2016.jpg',
+		scores: [3, 5, 4, 4, 1, 3, 5, 1, 5, 4]
+	},{
+		name: 'Anthony Bourdain',
+		photo: 'https://pbs.twimg.com/profile_images/855122963674390529/0AMZgedO.jpg',
+		scores: [4, 2, 5, 4, 1, 1, 5, 3, 5, 3]
+	},{
+		name: 'Donald Trump',
+		photo: 'http://celebrityinsider.org/wp-content/uploads/2017/10/Donald-Trump-Vanity-Fair-.jpg',
+		scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+	},{
+		name: 'Miley Cyrus',
+		photo: 'https://chairnerd.global.ssl.fastly.net/tba/wp-content/uploads/2013/12/miley-cyrus-72115.jpg',
+		scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+    },{
+		name: 'Bender',
+		photo: 'https://www.wired.com/images_blogs/underwire/2011/01/robot_roundup_07-660x465.jpg',
+		scores: [1, 5, 5, 5, 1, 5, 5, 5, 5, 1]
+    }];
+
+module.exports = friends;
---
+++ server.js
@@ -0,0 +1,26 @@
+
+// These are our dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+
+var app = express();
+var PORT = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type:'application/vnd.api+json'}));
+
+
+
+// This is our Routing
+require('./app/routing/apiRoutes.js')(app); 
+require('./app/routing/htmlRoutes.js')(app);
+
+
+// Starts the server to begin listening on whatever port you determine. 
+app.listen(process.env.PORT || 3000, function(){
+	console.log('App listening on PORT ' + process.env.PORT);
+});
